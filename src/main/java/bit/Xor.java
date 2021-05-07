fc18bfaeb848a84d339794b163237eb3ec6416ce
lufengxiang <lufengxiang@Znlh.com>
2021-05-07 10:09:02 +0800

    'gitignore'

---
+++ src/main/java/bit/Xor.java
@@ -0,0 +1,26 @@
+package bit;
+
+/**
+ * @author lufengxiang
+ * @since 2021/5/7
+ **/
+public class Xor {
+    public int xorOperation(int n, int start) {
+        int s = start >> 1, e = n & start & 1;
+        int ret = sumXor(s - 1) ^ sumXor(s + n - 1);
+        return ret << 1 | e;
+    }
+
+    public int sumXor(int x) {
+        if (x % 4 == 0) {
+            return x;
+        }
+        if (x % 4 == 1) {
+            return 1;
+        }
+        if (x % 4 == 2) {
+            return x + 1;
+        }
+        return 0;
+    }
+}
